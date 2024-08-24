0825aeb9ec45856682d3be777184bcd337a56226
paulo <paulorccaraujo@gmail.com>
2024-08-24 11:14:29 +0200

    Improve diagnostics

--- nvim/.config/nvim/lua/display.lua
+++ nvim/.config/nvim/lua/display.lua
@@ -2,6 +2,43 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+-- Customize how diagnostics are displayed
+-- From https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#customizing-how-diagnostics-are-displayed
+-- These are currently the default values
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+-- Change diagnostic symbols in the sign column
+-- From https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#change-diagnostic-symbols-in-the-sign-column-gutter
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Show vim diagnostics in a floating window instead of inline
+-- From https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = bufnr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = 'rounded',
+      source = 'always',
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end
+})
+
 -- Rainbow parenthesis configuration
 vim.g.rainbow_active = 1
 vim.g.rainbow_conf = {
